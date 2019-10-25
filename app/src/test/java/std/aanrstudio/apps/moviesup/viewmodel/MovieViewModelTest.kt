4b1b0179556e77eb72f8094de079dfe6f8dbc9ee
aanr <rnaufal32@gmail.com>
2019-10-25 19:11:21 +0700

    Revisi Submission 2

--- app/src/test/java/std/aanrstudio/apps/moviesup/viewmodel/MovieViewModelTest.kt
+++ app/src/test/java/std/aanrstudio/apps/moviesup/viewmodel/MovieViewModelTest.kt
@@ -9,9 +9,16 @@ import std.aanrstudio.apps.moviesup.data.source.MovieRepository
 import std.aanrstudio.apps.moviesup.data.source.model.Movie
 import std.aanrstudio.apps.moviesup.ui.fragment.movie.MovieViewModel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.Rule
 import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+import std.aanrstudio.apps.moviesup.data.source.mock.MovieMockData
+import std.aanrstudio.apps.moviesup.data.source.remote.RemoteRepository
 
 
 @RunWith(JUnit4::class)
@@ -20,19 +27,27 @@ class MovieViewModelTest {
     @Rule @JvmField
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    lateinit var movieViewModel: MovieViewModel
+    private var movieViewModel: MovieViewModel? = null
     private var repository = mock(MovieRepository::class.java)
 
     @Before
     fun before() {
+        MockitoAnnotations.initMocks(this)
         movieViewModel = MovieViewModel(repository)
     }
 
     @Test
     fun getMovies() {
-        val dummy:MutableLiveData<ArrayList<Movie>> = MutableLiveData()
+        val list = MovieMockData.mockData
+        val dummy: MutableLiveData<ArrayList<Movie>> = MutableLiveData()
+        dummy.value = list
 
         `when`(repository.getMovie()).thenReturn(dummy)
-        verify(repository).getMovie()
+
+        val observer = mock(Observer::class.java)
+
+        movieViewModel?.movieList?.observeForever(observer as Observer<in ArrayList<Movie>>)
+
+        verify(observer)
     }
 }
